87be89805c2f139cd9a5eb108116442e6ac91345
賴偉彥 <smh920055@gmail.com>
2017-03-07 19:53:28 +0800

    HomeWork Session1

--- WebApp_HomeWork/WebApp_HomeWork/Controllers/HomeController.cs
+++ WebApp_HomeWork/WebApp_HomeWork/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp_HomeWork.Models;
 
 namespace WebApp_HomeWork.Controllers
 {
@@ -25,6 +26,33 @@ namespace WebApp_HomeWork.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult MoneyList()
+        {
+            List<MoneyClass> model = new List<MoneyClass>();
+            model.Add(new MoneyClass
+            {
+                category = "支出",
+                money = "6600",
+                date = "2017-03-05",
+                description = "旅遊"
+            });
+            model.Add(new MoneyClass
+            {
+                category = "收入",
+                money = "66000",
+                date = "2017-03-05",
+                description = "薪資"
+            });
+            model.Add(new MoneyClass
+            {
+                category = "支出",
+                money = "600",
+                date = "2017-03-05",
+                description = "交通"
+            });
+            return View(model);
         }
     }
 }
---
+++ WebApp_HomeWork/WebApp_HomeWork/Models/MoneyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_HomeWork.Models
+{
+    public class MoneyClass
+    {
+        public string category { get; set; }
+
+        public string money { get; set; }
+
+        public string date { get; set; }
+
+        public string description { get; set; }
+    }
+}
