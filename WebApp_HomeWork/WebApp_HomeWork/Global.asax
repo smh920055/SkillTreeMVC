a9b37fbbf3d047ea91a85e8eade2bf83c38bc140
賴偉彥 <smh920055@gmail.com>
2017-03-07 19:36:26 +0800

    HomeWork

---
+++ WebApp_HomeWork/WebApp_HomeWork/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp_HomeWork.MvcApplication" Language="C#" %>
---
+++ WebApp_HomeWork/WebApp_HomeWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_HomeWork.Startup))]
+namespace WebApp_HomeWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
