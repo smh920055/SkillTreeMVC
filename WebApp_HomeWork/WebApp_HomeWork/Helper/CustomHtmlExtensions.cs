0691838323d6a04e596fc1f27b8f9fde3b07df84
賴偉彥 <smh920055@gmail.com>
2017-03-24 21:47:09 +0800

    列表顏色變換，改用HtmlHelper擴充方法實作

---
+++ WebApp_HomeWork/WebApp_HomeWork/Helper/CustomHtmlExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp_HomeWork.Models;
+
+namespace WebApp_HomeWork.Helper
+{
+    public static class CustomHtmlExtensions
+    {
+        public static string ShowCategoryColor(this HtmlHelper helper, CategoryType category)
+        {
+            if (category == CategoryType.收入)
+                return "blue";
+            else
+                return "red";
+        }
+    }
+}
